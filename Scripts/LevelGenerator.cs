0a06983bd9cf9b3a38499f76c7d37b9763b4129a
Jeremiah Michaels <jam.jeremiah88@gmail.com>
2024-10-07 00:46:34 -0400

    Added blobs as a way to provide variation to level generation

--- Scripts/LevelGenerator.cs
+++ Scripts/LevelGenerator.cs
@@ -1,20 +1,28 @@
 using Godot;
 using System;
-using System.Runtime.CompilerServices;
-using System.Security.AccessControl;
 using System.Collections.Generic;
-using System.Net.Http.Headers;
-using System.Linq;
-
 public partial class LevelGenerator : Node2D
 {
     [ExportGroup("Level Properties")]
     [Export]
-    public Vector2 LevelSize { get; set; } = new Vector2(20, 20);
+    public Vector2 LevelSize { get; set; } = new Vector2(40, 40);
     [Export]
     public int PlatformCount { get; set; } = 20;
     [Export]
     public int PlatformGaps { get; set; } = 6;
+    [Export]
+    public int Smoothness { get; set; } = 3;
+    [Export]
+    public int Blobs { get; set; } = 2;
+    [Export]
+    public Vector2 FrameBlockMinSize { get; set; } = new Vector2(5, 5);
+    [Export]
+    public Vector2 FrameBlockMaxSize { get; set; } = new Vector2(15, 10);
+    [ExportGroup("Blob Properties")]
+    [Export]
+    public Vector2 BlobMaxSize { get; set; } = new Vector2(7, 7);
+    [Export]
+    public Vector2 BlobMinSize { get; set; } = new Vector2(5, 5);
 
     private TileMapLayer _frameTileMap;
     private TileMapLayer _platformTileMap;
@@ -53,6 +61,63 @@ public partial class LevelGenerator : Node2D
                 _frameTileMap.SetCell(new Vector2I((int)LevelSize.X, y), 0, new Vector2I(2, 2));
             }
         }
+        GenerateSides();
+        for (int i=0; i < Blobs; i++) GenerateBlob();
+    }
+
+    private void GenerateSides()
+    {
+        int n = 0;
+        while (n <= LevelSize.Y)
+        {
+            n += (int)GenerateBlock(new Vector2(0, n), FrameBlockMinSize, FrameBlockMaxSize).Y/2;
+        }
+        n = 0;
+        while (n <= LevelSize.Y)
+        {
+            n += (int)GenerateBlock(new Vector2(LevelSize.X, n), FrameBlockMinSize, FrameBlockMaxSize).Y/2;
+        }
+        n = 0;
+        while (n <= LevelSize.X)
+        {
+            n += (int)GenerateBlock(new Vector2(n, 0), FrameBlockMinSize, FrameBlockMaxSize).X/2;
+        }
+        n = 0;
+        while (n <= LevelSize.X)
+        {
+            n += (int)GenerateBlock(new Vector2(n, LevelSize.Y), FrameBlockMinSize, FrameBlockMaxSize).X/2;
+        }
+    }
+
+    private void GenerateBlob()
+    {
+        Vector2 position = LevelSize/2 + new Vector2((float)GD.RandRange(-LevelSize.X/3, LevelSize.X/3), (float)GD.RandRange(-LevelSize.Y/3, LevelSize.Y/3));
+        for(int i = 0; i < 5; i++)
+        {
+            GenerateBlock(new Vector2(position.X + GD.RandRange(-3, 3), position.Y + GD.RandRange(-3, 3)), BlobMinSize, BlobMaxSize);
+        }
+    }
+
+    private Vector2 GenerateBlock(Vector2 pos, Vector2 min, Vector2 max)
+    {
+        Vector2 size = new Vector2((float)GD.RandRange(min.X, max.X), (float)GD.RandRange(min.Y, max.Y));
+
+        for(int i = 0; i < Smoothness; i++) {
+            size = new Vector2(Math.Min(size.X, (float)GD.RandRange(min.X, max.X)), Math.Min(size.Y, (float)GD.RandRange(min.Y, max.Y)));
+        }
+
+        for (int x = 0; x <= size.X; x++)
+        {
+            for (int y = 0; y <= size.Y; y++)
+            {
+                Vector2I position = new Vector2I(x + (int)(pos.X-size.X/2), y + (int)(pos.Y-size.Y/2));
+                if (position.X < LevelSize.X && position.Y < LevelSize.Y)
+                {
+                    _frameTileMap.SetCell(position, 0, new Vector2I(2, 2));
+                }
+            }
+        }
+        return size;
     }
 
     /// <summary>
@@ -101,6 +166,13 @@ public partial class LevelGenerator : Node2D
             for (int y = 0; y < buffer.Size.Y; y++)
             {
                 TileData tile = _platformTileMap.GetCellTileData(new Vector2I(x, y) + buffer.Position); // Get the tile information at x, y
+
+                if(tile != null) // If the tile exists
+                {
+                    return false;
+                }
+
+                tile = _frameTileMap.GetCellTileData(new Vector2I(x, y) + buffer.Position); // Get the tile information at x, y
 
                 if(tile != null) // If the tile exists
                 {
