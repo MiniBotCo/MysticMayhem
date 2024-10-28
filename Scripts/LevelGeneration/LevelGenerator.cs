53351e23653c74d621f2f627cc21f7051ecb49b7
Jeremiah Michaels <jam.jeremiah88@gmail.com>
2024-10-28 16:06:41 -0400

    Changed EnemySpawner into a basic Spawner node that can spawn anything in group "Spawnable"

--- Scripts/LevelGeneration/LevelGenerator.cs
+++ Scripts/LevelGeneration/LevelGenerator.cs
@@ -8,14 +8,14 @@ public partial class LevelGenerator : Node2D
 
     private FrameGenerator _frameGenerator;
     private PlatformGenerator _platformGenerator;
-    private EnemySpawner _enemySpawner;
+    private Spawner _enemySpawner;
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
         _frameGenerator = GetNode<FrameGenerator>("FrameGenerator");
         _platformGenerator = GetNode<PlatformGenerator>("PlatformGenerator");
-        _enemySpawner = GetNode<EnemySpawner>("EnemySpawner");
+        _enemySpawner = GetNode<Spawner>("EnemySpawner");
 
         base._Ready();
 
@@ -31,15 +31,13 @@ public partial class LevelGenerator : Node2D
         _frameGenerator.GenerateFrame();
 
         _frameGenerator.GenerationComplete += () => GD.Print("Generation complete");
-        //await ToSignal(_frameGenerator, FrameGenerator.SignalName.GenerationComplete);
 
         _platformGenerator.AddCheckLayer(_frameGenerator.GetTileMapLayer());
 
         _platformGenerator.SetBounds(LevelSize);
         _platformGenerator.GeneratePlatforms();
 
-        //await ToSignal(_platformGenerator, Node.SignalName.Ready);
         _enemySpawner.levelSize = LevelSize;
-        _enemySpawner.SpawnEnemies(_platformGenerator.GetTileMapLayer());
+        _enemySpawner.Spawn(_platformGenerator.GetTileMapLayer());
     }
 }
