d6f36bf4e4742248bf2ce5a3c70f3753a6521fd3
Tyler Sherry <tcsmq2@gmail.com>
2024-10-29 21:34:08 -0500

    Added animations for walking, attack, and started building state machines

---
+++ Scripts/Character/StateMachine.cs
@@ -0,0 +1,8 @@
+using Godot;
+using System;
+
+public partial class StateMachine : Node
+{
+    [Export] private Node currentState;
+    [Export] private Node[] states;
+}
---
+++ Scripts/General/GameConstants.cs
@@ -0,0 +1,14 @@
+public class GameConstants
+{
+    //Animations
+    public const string ANIM_IDLE = "Idle";
+    public const string ANIM_MOVE = "Move";
+    public const string ANIM_ATTACK = "Attack";
+
+
+    //Inputs
+    public const string INPUT_MOVE_LEFT = "MoveLeft";
+    public const string INPUT_MOVE_RIGHT = "MoveRight";
+    public const string INPUT_JUMP = "Jump";
+    public const string INPUT_ATTACK = "Attack";
+}
